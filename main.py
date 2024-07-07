9c3cb9514f5f78850f7ae015dcdf8a15e546f41e
Fuyuki Malahom <fuyuki325@gmail.com>
2024-07-06 18:10:26 -0700

    Completed bot

--- main.py
+++ main.py
@@ -1,7 +1,87 @@
 import os
 from dotenv import load_dotenv
+from typing import Final
+from discord import Intents, Client, Message
+from responses import get_response
+from random import choice, randint
 
+# load token
 load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
-print(DISCORD_TOKEN)
+
+# BOT SETUP
+intents: Intents = Intents.default()
+intents.message_content = True # NOQA
+client: Client = Client(intents=intents)
+
+keyWords = ['gf', 'girlfriend']
+# STEP 2: Message functionality
+async def send_message(message: Message, user_message: str) -> None:
+    username = str(message.author)
+    if not user_message:
+        print('(Message was empty because intents were not enabled probably)')
+        return
+
+    if any(word in user_message for word in keyWords):
+        await message.channel.send('no gf until marriage')
+        return
+
+    if 'im' in user_message :
+        slicedMessage = user_message[3:]
+        await message.channel.send('Nice to meet you ' + slicedMessage + '! Sorry, I have a boyfriend by the way')
+        return
+
+    if 'date' in user_message:
+        await message.channel.send('Nice to meet you ' + username + '! Sorry, I\'m currently taken by Fuyuki the way')
+        return
+    if 'hello' in user_message:
+        await message.channel.send('Hello there!')
+        return
+    if 'hi' in user_message:
+        await message.channel.send('Hello there!')
+        return
+    if 'roll dice' in user_message:
+        await message.channel.send(f'You rolled: {randint(1, 6)}')
+        return
+
+    # 49/50 chance it won't say anything
+    if randint(1, 50) < 50:
+        return
+
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message, message)
+        if is_private:
+            await message.author.send(response)
+        else:
+            await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+# STEP 3: Handle startup
+@client.event
+async def on_ready() -> None:
+   print(f'{client.user} is now running!')
+
+
+# STEP 4: handling incoming messages
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    # print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+# STEP 5: Main entry point
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
--- responses.py
+++ responses.py
@@ -1,2 +1,102 @@
-def get_response(user_input: str) -> str:
-    raise NotImplementedError('Code not finished')
+from random import choice, randint
+from discord import Intents, Client, Message
+
+def get_response(user_input: str, message: Message) -> str:
+    lowered: str = user_input.lower()
+    username = str(message.author)
+
+    if lowered == '':
+        return 'Well, you\'re awfully silent...'
+
+    if username == 'linlinhere':
+        return f'lin. You are such a naughty boy. stop watching that.'
+
+    if username == 'bra.na.don':
+        switch = randint(1,6)
+        if switch == 1:
+            return "Get off Valorant and do something productive in your life. Brainrot"
+        elif switch == 2:
+            return "It's drone phase. Not TikTok phase"
+        elif switch == 3:
+            return "Sticking out " + username + "'s gyatt for the rizzler!"
+        elif switch == 4:
+            return "Please keep the hentai out of the chat"
+        elif switch == 5:
+            return "I KNOW you didn't shower! Take a bath. my god"
+        elif switch == 6:
+            return "So we getting married when?"
+
+    if username == 'justinhoffner':
+        switch = randint(1,6)
+        if switch == 1:
+            return "Beating up kids in a video game is not a flex Justin"
+        elif switch == 2:
+            return "Lowkey Justin got that gyatt. I've seen it"
+        elif switch == 3:
+            return "Sticking out " + username + "'s gyatt for the rizzler!"
+        elif switch == 4:
+            return "Justin gimme doordash"
+        elif switch == 5:
+            return "Meow! meow meow"
+        elif switch == 6:
+            return "Justin Minecraft super Minecraft"
+
+    if username == 'jasonayuen':
+        switch = randint(1,6)
+        if switch == 1:
+            return "Jason girlfriend when?"
+        elif switch == 2:
+            return "Jason speaking facts rn. on god"
+        elif switch == 3:
+            return "Sticking out " + username + "'s gyatt for the rizzler!"
+        elif switch == 4:
+            return "Love you my peekaboo Jason"
+        elif switch == 5:
+            return "I would pay $10 a month to see them feet"
+        elif switch == 6:
+            return "Jason wants to play Valorant rn."
+
+    if username == 'ahleckx':
+        switch = randint(1,6)
+        if switch == 1:
+            return "Famous youtuber alert!! Subscribe to alecksxd on YouTube"
+        elif switch == 2:
+            return "Daily Vlogs is back boys"
+        elif switch == 3:
+            return "Sticking out " + username + "'s gyatt for the rizzler!"
+        elif switch == 4:
+            return "Love you my peekaboo Alex"
+        elif switch == 5:
+            return "Alex wants to play Minecraft rn. I can feel it."
+        elif switch == 6:
+            return "Alex JUST CALLED YOU A BITCH!"
+
+    if username == 'natepin':
+        switch = randint(1,6)
+        if switch == 1:
+            return "NATHAN NATHAN NATHAN NATHAN"
+        elif switch == 2:
+            return "Nathan is my superhero"
+        elif switch == 3:
+            return "Sticking out " + username + "'s gyatt for the rizzler!"
+        elif switch == 4:
+            return "Love you my peekaboo Nathan"
+        elif switch == 5:
+            return "Nathan def tryna get on League rn. on god."
+        elif switch == 6:
+            return "Shut up and make me a sandwich"
+
+    else:
+        return choice(['Jason better stop yapping',
+                       'Love you Brandon',
+                       'My peekaboo Jushin',
+                       'Skibidi Toilet',
+                       'Is Jason the goat?',
+                       'Brandon likes feet',
+                       'Get your money up. not your funny up',
+                       'So we\'re in copper?',
+                       'fucking Justin man',
+                       'fucking alex bro',
+                       'fuck you',
+                       'at least i didn\'t get rejected at prom'
+                       'bro hit the green val rank and now he thinks he\'s the goat'])
