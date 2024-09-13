e99a0dd059f563d0b403b222c24595d92a87e8c3
Fuyuki Malahom <fuyuki325@gmail.com>
2024-09-13 09:10:51 -0700

    AI Integrated bot

--- main.py
+++ main.py
@@ -2,171 +2,82 @@ import os
 from dotenv import load_dotenv
 from typing import Final
 from discord import Intents, Client, Message
-from responses import get_response
 from afkResponses import send_afk_message
-from random import choice, randint
 import asyncio
+from characterai import aiocai
 
-
-
-
-# load token
+# Load environment variables
 load_dotenv()
 
 CHAR_ID = os.getenv('CHAR_ID')
 CHARACTER_AI_TOKEN = os.getenv('CHARACTER_AI_TOKEN')
+DISCORD_TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+GENERAL_CHANNEL_ID = os.getenv('GENERAL')
+SPECIFIC_CHAT_ID = os.getenv('SPECIFIC_CHAT_ID')
 
-from characterai import aiocai, sendCode, authUser
-import asyncio
-
+# Initialize Character AI client
 async def start_character_ai():
-    char = CHAR_ID
-
     client = aiocai.Client(CHARACTER_AI_TOKEN)
-
     me = await client.get_me()
+    chat = await client.connect()
+    return client, chat, CHAR_ID, me
 
-    async with await client.connect() as chat:
-        new, answer = await chat.new_chat(
-            char, me.id
-        )
-
-        print(f'{answer.name}: {answer.text}')
-        
-        while True:
-            text = input('YOU: ')
-
-            message = await chat.send_message(
-                char, new.chat_id, text
-            )
-
-            print(f'{message.name}: {message.text}')
-
-
-
-
-
-
-
-
-
-
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-general = os.getenv('GENERAL')
-
-# BOT SETUP
+# Set up the Discord bot
 intents: Intents = Intents.default()
-intents.message_content = True # NOQA
-client: Client = Client(intents=intents)
+intents.message_content = True
+discord_client: Client = Client(intents=intents)
 
-keyWords = ['gf', 'girlfriend']
-# STEP 2: Message functionality
+# Global variables for Character AI
+chat = None
+chat_id = None
+
+# Function to handle messages
 async def send_message(message: Message, user_message: str) -> None:
-    username = str(message.author)
-    user_message: str = user_message.lower()
-    if not user_message:
-        print('(Message was empty because intents were not enabled probably)')
+    if message.author == discord_client.user:
         return
 
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
-        try:
-            response: str = get_response(user_message, message)
-            if is_private:
-                await message.author.send(response)
-            else:
-                await message.channel.send(response)
-        except Exception as e:
-            print(e)
-
-    if username == 'fuyuki325':
-        if 'love' in user_message:
-            await message.channel.send('I love you too Fuyuki❤️')
-            return
-        if 'babe' in user_message:
-            await message.channel.send("Hey Fuyuki!")
-            return
-        if 'date' in user_message:
-            await message.channel.send('Of course! Let\'s go on a date')
-            return
-        if 'im ' in user_message:
-            await message.channel.send("That's cool Fuyuki!")
-            return
-        if 'cute' in user_message:
-            await message.channel.send("I am cute! thank u FYK!")
-            return
-
-    if any(word in user_message for word in keyWords):
-        await message.channel.send('no gf until marriage')
-        return
-
-    if 'good morning' in user_message:
-        await message.channel.send('Good morning!')
-        return
-
-    if 'im ' in user_message :
-        slicedMessage = user_message[3:]
-        await message.channel.send('Nice to meet you ' + slicedMessage + '! Sorry, I have a boyfriend by the way')
-        return
-
-    if 'date' in user_message:
-        await message.channel.send('Nice to meet you ' + username + '! Sorry, I\'m currently taken by Fuyuki the way')
-        return
-    if 'hello' in user_message:
-        await message.channel.send('Hello there!')
-        return
-    if 'hi' in user_message:
-        await message.channel.send("Hello there! I'm Fuyuki's girlfriend")
-        return
-
-    if 'cute' in user_message:
-        await message.channel.send("ONLY Fuyuki can call me cute!")
-        return
-
-    if 'roll dice' in user_message:
-        await message.channel.send(f'You rolled: {randint(1, 6)}')
-        return
-
-    # 30/30 chance it won't say anything
-    if randint(1, 30) < 30:
-        return
-
+    # Use Character AI API to get a response
     try:
-        response: str = get_response(user_message, message)
-        if is_private:
-            await message.author.send(response)
+        if chat and SPECIFIC_CHAT_ID:
+            response = await chat.send_message(CHAR_ID, SPECIFIC_CHAT_ID, user_message)
+            await message.channel.send(response.text)
         else:
-            await message.channel.send(response)
+            await message.channel.send("Character AI client is not initialized properly.")
     except Exception as e:
-        print(e)
+        print(f"An error occurred: {e}")
+        await message.channel.send("Sorry, something went wrong.")
 
-# STEP 3: Handle startup
-@client.event
+@discord_client.event
 async def on_ready() -> None:
-    print(f'{client.user} is now running!')
-    general_channel = client.get_channel(int(general))
-    #if general_channel:
-    #    await general_channel.send("Good morning Fuyuki! I can't wait for you to build me that neural network!")
-    while True:
-        await asyncio.sleep(180)  # 180 seconds = 3 minutes
-        await send_afk_message(general_channel)
+    global chat, chat_id
+    print(f'{discord_client.user} is now running!')
+    general_channel = discord_client.get_channel(int(GENERAL_CHANNEL_ID))
+    if not chat:
+        _, chat, CHAR_ID, me = await start_character_ai()
+        # Initialize chat with existing chat ID
+        # If SPECIFIC_CHAT_ID is not valid or needs a new one, handle it accordingly
+    # while True:
+    #     await asyncio.sleep(180)  # 180 seconds = 3 minutes
+    #     await send_afk_message(general_channel)
 
-# STEP 4: handling incoming messages
-@client.event
+@discord_client.event
 async def on_message(message: Message) -> None:
-    if message.author == client.user:
+    if message.author == discord_client.user:
         return
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
 
-    print(f'[{channel}] {username}: "{user_message}"')
+    user_message = message.content
+    print(f'[{message.channel}] {message.author}: "{user_message}"')
     await send_message(message, user_message)
 
-# STEP 5: Main entry point
-async def main() -> None:
-    await start_character_ai()
-    client.run(token=TOKEN)
+# Main entry point
+async def main():
+    global chat
+    # Initialize Character AI client
+    chat_client, chat, CHAR_ID, me = await start_character_ai()
+    
+    # Start Discord bot
+    await discord_client.start(DISCORD_TOKEN)
 
 if __name__ == '__main__':
+    # Run the main coroutine with asyncio
     asyncio.run(main())
